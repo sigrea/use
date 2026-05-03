0c8feddb7096582405c9e393de943e2e73f744d7
aose-yuu <aose.developer@gmail.com>
2026-05-03 11:34:28 +0900

    chore: expose the package entry for npm consumers

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-export {};
+export { useCounter } from "./packages/use/useCounter";
+export { useEventListener } from "./packages/use/useEventListener";
+export { useIntervalFn } from "./packages/use/useIntervalFn";
+export { useMediaQuery } from "./packages/use/useMediaQuery";
+export { useTimeoutFn } from "./packages/use/useTimeoutFn";
+export { useToggle } from "./packages/use/useToggle";
+export { useWindowSize } from "./packages/use/useWindowSize";
+export type {
+	DocumentLike,
+	MatchMediaWindow,
+	MaybeTarget,
+	MaybeValue,
+	NavigatorLike,
+	TargetEventMap,
+	UseCounterOptions,
+	UseCounterReturn,
+	UseEventListenerOptions,
+	UseEventListenerReturn,
+	UseIntervalFnOptions,
+	UseIntervalFnReturn,
+	UseMediaQueryOptions,
+	UseMediaQueryReturn,
+	UseTimeoutFnOptions,
+	UseTimeoutFnReturn,
+	UseToggleOptions,
+	UseToggleReturn,
+	ValueGetter,
+	WindowLike,
+	WindowSizeLike,
+	UseWindowSizeOptions,
+	UseWindowSizeReturn,
+} from "./packages/use/types";
